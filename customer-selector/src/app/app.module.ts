576a5fc0704b688321d6f09e1be9131638013e46
Harry <harry74323@gmail.com>
2018-03-08 15:04:49 +1100

    create customerDetail

--- customer-selector/src/app/app.module.ts
+++ customer-selector/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerListComponent
+    CustomerListComponent,
+    CustomerDetailComponent
   ],
   imports: [
     BrowserModule
